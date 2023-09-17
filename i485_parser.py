4b0045a058db128a8f2445941e85569888269818
PXMYH <pxmyhdev@gmail.com>
2023-09-17 02:44:37 +0000

    Update parser, to select dates using beautifulsoup has some issues

--- i485_parser.py
+++ i485_parser.py
@@ -12,13 +12,14 @@ base_url = "https://travel.state.gov/content/travel/en/legal/visa-law0/visa-bull
 
 # Define the years to loop through
 start_year = 2016
-current_year = 2023  # You can adjust this as needed
+current_year = 2016  # You can adjust this as needed
 
 # Define the months
-months = [
-    "january", "february", "march", "april", "may", "june", "july", "august",
-    "september", "october", "november", "december"
-]
+# months = [
+#     "january", "february", "march", "april", "may", "june", "july", "august",
+#     "september", "october", "november", "december"
+# ]
+months = ["january"]
 
 # Loop through the years and months
 for year in range(start_year, current_year + 1):
@@ -33,6 +34,7 @@ for year in range(start_year, current_year + 1):
 
         # Send an HTTP GET request to the URL
         response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
 
         # Check if the request was successful (status code 200)
         if response.status_code == 200:
@@ -42,7 +44,16 @@ for year in range(start_year, current_year + 1):
             # Save the page content to a file
             filename = f"visa_bulletin_pages/{month}_{calendar_year}.html"
             with open(filename, "w", encoding="utf-8") as file:
-                file.write(str(soup))
+                file.write(str(soup.prettify()))
             print(f"Downloaded: {month} {calendar_year}")
+
+            print(f"response.text = {response.text}")
+
+            # Extract the date using the specified CSS selector
+            section_selector = "body > div.tsg-rwd-body-frame-row > div.contentbody > div.tsg-rwd-main-copy-frame > div.tsg-rwd-main-copy-body-frame.withrail > div.tsg-rwd-content-page-parsysxxx.parsys > div:nth-child(5) > div"
+            employment_based_element = soup.css.select(section_selector)
+            print(f"employment_based_element = {employment_based_element}")
+            # date = date_element.text.strip(
+            # ) if date_element else 'Date not found'
         else:
             print(f"Failed to download: {month} {calendar_year}")
