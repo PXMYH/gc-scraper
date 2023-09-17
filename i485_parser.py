df80decd7046c91678449fa3ddc1cc75443df9d4
PXMYH <pxmyhdev@gmail.com>
2023-09-17 03:23:10 +0000

    Update parser to write a csv file contains the PD date change progress

--- i485_parser.py
+++ i485_parser.py
@@ -1,7 +1,7 @@
 import os
-
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 # Create a directory to store the downloaded pages
 if not os.path.exists('visa_bulletin_pages'):
@@ -19,7 +19,12 @@ months = [
     "january", "february", "march", "april", "may", "june", "july", "august",
     "september", "october", "november", "december"
 ]
-# months = ["january"]
+
+# Create a CSV file to store the extracted dates
+csv_filename = "visa_bulletin_dates.csv"
+
+# Initialize a list to store the data
+data = []
 
 # Loop through the years and months
 for year in range(start_year, current_year + 1):
@@ -34,28 +39,34 @@ for year in range(start_year, current_year + 1):
 
         # Send an HTTP GET request to the URL
         response = requests.get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
 
         # Check if the request was successful (status code 200)
         if response.status_code == 200:
             # Parse the HTML content of the page
             soup = BeautifulSoup(response.text, "html.parser")
 
-            # Save the page content to a file
-            filename = f"visa_bulletin_pages/{month}_{calendar_year}.html"
-            with open(filename, "w", encoding="utf-8") as file:
-                file.write(str(soup.prettify()))
-            print(f"Downloaded: {month} {calendar_year}")
-
             # Extract the date using the specified CSS selector
-            # The EB catagory table is the first of three tables after selection
-            # and the date is the 3rd in the table
-            # verified this all the way back to 2016 and it has been the case
             date_selector = "body > div.tsg-rwd-body-frame-row > div.contentbody > div.tsg-rwd-main-copy-frame > div.tsg-rwd-main-copy-body-frame.withrail > div.tsg-rwd-content-page-parsysxxx.parsys > div:nth-child(5) > div > p > table > tbody > tr:nth-child(4) > td:nth-child(3)"
-            date_element = soup.css.select_one(date_selector)
+            date_element = soup.select_one(date_selector)
 
             date = date_element.text.strip(
             ) if date_element else 'Date not found'
             print(f"EB3 For China Mainland born PD date is {date}")
+
+            # Interpolate the "Year-Month" column name
+            year_month = f"{calendar_year}-{month_index + 1:02}"  # Format as "year-month"
+            data.append((year_month, date))
         else:
             print(f"Failed to download: {month} {calendar_year}")
+
+# Write the extracted dates to a CSV file
+with open(csv_filename, 'w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+
+    # Write the header row with the dynamically interpolated column name
+    csv_writer.writerow(["Year-Month", "Date"])
+
+    # Write the data rows
+    csv_writer.writerows(data)
+
+print(f"Data written to {csv_filename}")
