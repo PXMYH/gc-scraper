2af3e62ca3d7625c4c946c69263a350a571e976c
PXMYH <pxmyhdev@gmail.com>
2023-09-17 03:16:23 +0000

    Fixed css selector issue
    The issue is the css selector get directly from Chrome DevTool is missing a "p" in between div and table, not sure why that's the case as looking directly on the HTML can't explain it, but it is the case that's causing previously unable to extract the date out if using the CSS selector directly

--- i485_parser.py
+++ i485_parser.py
@@ -15,11 +15,11 @@ start_year = 2016
 current_year = 2016  # You can adjust this as needed
 
 # Define the months
-# months = [
-#     "january", "february", "march", "april", "may", "june", "july", "august",
-#     "september", "october", "november", "december"
-# ]
-months = ["january"]
+months = [
+    "january", "february", "march", "april", "may", "june", "july", "august",
+    "september", "october", "november", "december"
+]
+# months = ["january"]
 
 # Loop through the years and months
 for year in range(start_year, current_year + 1):
@@ -47,13 +47,15 @@ for year in range(start_year, current_year + 1):
                 file.write(str(soup.prettify()))
             print(f"Downloaded: {month} {calendar_year}")
 
-            print(f"response.text = {response.text}")
-
             # Extract the date using the specified CSS selector
-            section_selector = "body > div.tsg-rwd-body-frame-row > div.contentbody > div.tsg-rwd-main-copy-frame > div.tsg-rwd-main-copy-body-frame.withrail > div.tsg-rwd-content-page-parsysxxx.parsys > div:nth-child(5) > div"
-            employment_based_element = soup.css.select(section_selector)
-            print(f"employment_based_element = {employment_based_element}")
-            # date = date_element.text.strip(
-            # ) if date_element else 'Date not found'
+            # The EB catagory table is the first of three tables after selection
+            # and the date is the 3rd in the table
+            # verified this all the way back to 2016 and it has been the case
+            date_selector = "body > div.tsg-rwd-body-frame-row > div.contentbody > div.tsg-rwd-main-copy-frame > div.tsg-rwd-main-copy-body-frame.withrail > div.tsg-rwd-content-page-parsysxxx.parsys > div:nth-child(5) > div > p > table > tbody > tr:nth-child(4) > td:nth-child(3)"
+            date_element = soup.css.select_one(date_selector)
+
+            date = date_element.text.strip(
+            ) if date_element else 'Date not found'
+            print(f"EB3 For China Mainland born PD date is {date}")
         else:
             print(f"Failed to download: {month} {calendar_year}")
